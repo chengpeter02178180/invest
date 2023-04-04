6d9e6242cec13655b0e52a23e0790d3198ccfa1c
chengpeter02178180 <129818885+chengpeter02178180@users.noreply.github.com>
2023-04-04 12:40:26 +0800

    Add files via upload

---
+++ .github/workflows/invest/Daily Closing Quotes - Market Statistical Information.py
@@ -0,0 +1,38 @@
+import requests as req
+import  pandas as pd
+import time
+import schedule
+
+
+def get_stock_dailytrade():
+    res=req.get("https://openapi.twse.com.tw/v1//exchangeReport/MI_INDEX")
+    df=pd.DataFrame(res.json())
+    localtime = time.localtime()
+    now= time.strftime("%Y-%m-%d %I-%M-%S", localtime)
+    df.to_csv(f"{now}.csv",encoding="utf_9_sig",index=False)
+    print(f"{now}取得股票資訊")
+
+schedule.every().day.at("18:00:00").do(get_stock_dailytrade)
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+
+
+
+
+
+def get_stock_everytrade():
+    res=req.get("https://openapi.twse.com.tw/v1/exchangeReport/STOCK_DAY_ALL")
+    df=pd.DataFrame(res.json())
+    localtime = time.localtime()
+    now= time.strftime("%Y-%m-%d %I-%M-%S", localtime)
+    df.to_csv(f"{now}.csv",encoding="utf_9_sig",index=False)
+    print(f"{now}取得股票資訊")
+
+schedule.every().day.at("06:00:00").do(get_stock_everytrade)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
